d6a97a3ef2fbf777f98158843cbab66d99a394df
DmKeller <dmit.keller@gmail.com>
2023-03-25 22:25:28 +0300

    finished task 2ю03

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var demoView: UIView!
+    
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    override func viewDidLayoutSubviews() {
+        demoView.layer.cornerRadius = 15
+        changedsSliderValue()
     }
-
-
+    
+    @IBAction func changedsSliderValue() {
+        
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let  blue = CGFloat(blueSlider.value)
+        
+        redValueLabel.text = redSlider.value.formatted()
+        greenValueLabel.text = greenSlider.value.formatted()
+        blueValueLabel.text = blueSlider.value.formatted()
+        
+        demoView.backgroundColor = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1
+        )
+    }
 }
 
